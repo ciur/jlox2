193c4c7b4ae598cc386404a33de5d40ff0698c1f
Eugen Ciur <eugen@papermerge.com>
2023-11-04 12:20:36 +0100

    minor adjustments

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.List;
 
 public class Main {
+    static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -19,10 +22,30 @@ public class Main {
         String source = new String(byteps, Charset.defaultCharset());
 
         run(source);
+
+        if (hadError) {
+            System.exit(65);
+        }
     }
 
     private static void run(String source) throws IOException {
         Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token: tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message
+        );
+        hadError = true;
     }
     private static void runPrompt() {
         System.out.println("runPrompt");
--- src/Scanner.java
+++ src/Scanner.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scanner {
     private final String source;
+    private final List<Token> tokens = new ArrayList<>();
 
     Scanner(String source) {
         System.out.println(source);
         this.source = source;
+    }
+
+    public List<Token> scanTokens() {
+        return tokens;
     }
 }
