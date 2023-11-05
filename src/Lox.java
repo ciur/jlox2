bf454ac8a091c7553c670b23372b231332324c77
Eugen Ciur <eugen@papermerge.com>
2023-11-05 15:59:17 +0100

    handle keywords and identifiers

--- src/Lox.java
+++ src/Lox.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
 import java.nio.file.Files;
@@ -47,7 +50,18 @@ public class Lox {
         );
         hadError = true;
     }
-    private static void runPrompt() {
-        System.out.println("runPrompt");
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print(">");
+            String line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+            run(line);
+        }
     }
 }
